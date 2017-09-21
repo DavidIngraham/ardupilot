ca7dab7c4fc819592ad0c21f2238af7d3262ed2d
David Ingraham <davingrahamd@gmail.com>
2017-09-21 16:46:51 -0700

    Begin Paraglider

---
+++ Tools/autotest/fg_paraglider_view.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Note: In order to view the paraglider model you must update to FlightGear 2017
+
+AUTOTESTDIR=$(dirname $0)
+
+nice fgfs \
+    --native-fdm=socket,in,10,,5503,udp \
+    --fdm=external \
+    --aircraft=paraglider \
+    --fg-aircraft="$AUTOTESTDIR/aircraft" \
+    --airport=YKRY \
+    --geometry=650x550 \
+    --bpp=32 \
+    --disable-anti-alias-hud \
+    --disable-hud-3d \
+    --disable-horizon-effect \
+    --timeofday=noon \
+    --disable-sound \
+    --disable-fullscreen \
+    --disable-random-objects \
+    --disable-ai-models \
+    --fog-disable \
+    --disable-specular-highlight \
+    --disable-anti-alias-hud \
+    --wind=0@0 \
+    $*
