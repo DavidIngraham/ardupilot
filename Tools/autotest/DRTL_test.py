dab120f446388006925e6d229742d627bd6ebd4d
David Ingraham <davingrahamd@gmail.com>
2018-04-18 09:58:25 -0700

    Tools: fix DRTL test SYSID

--- Tools/autotest/DRTL_test.py
+++ Tools/autotest/DRTL_test.py
@@ -14,13 +14,12 @@ import math
 from pymavlink import mavutil
 
 SYSID_OUT = 111
-COMPID_OUT = 0
 
-lat = -35.3620798 # CMAC runway north end
-lng = 149.1650704
-alt = 583.96
+lat_origin = -35.3620798 # CMAC runway north end
+lng_origin = 149.1650704
+alt_origin = 583.96
 
-UPDATE_PERIOD = 1 # send every second
+UPDATE_PERIOD = 100 # milliseconds
 
 def wait_heartbeat(m):
     '''wait for a heartbeat so we know the target system IDs'''
@@ -32,7 +31,7 @@ def wait_heartbeat(m):
 
 if __name__ == "__main__":
 
-	master = mavutil.mavlink_connection('udp:127.0.0.1:14551', SYSID_OUT, COMPID_OUT)
+	master = mavutil.mavlink_connection('udp:127.0.0.1:14551', source_system = SYSID_OUT)
 
 	# wait for the heartbeat msg to find the system ID
 	wait_heartbeat(master)
@@ -41,15 +40,20 @@ if __name__ == "__main__":
 
 	counter = 0
 
+	start_time = int(time.time()*1000)
+
 	while True:
-		if (time.time() - last_update) > UPDATE_PERIOD:
+		if (time.time()*1000 - last_update) > UPDATE_PERIOD:
 
-			lat += math.sin(counter/10)/10000
+			lat = lat_origin + math.sin(counter/100)/10000 # move up and down the cmac runway
+			lng = lng_origin
+			alt = alt_origin
 
-			print(lat)
+			print(int(time.time()*1000))
 
-			master.mav.heartbeat_send(31,3,0,0,4,2)
-			master.mav.global_position_int_send(time.time()/1000,lat, lng, alt, 0, 0, 0 , 0, 0)
+			if (counter % (1000/UPDATE_PERIOD) == 0): # send heartbeat at 1 Hz
+				master.mav.heartbeat_send(31,3,0,0,4,2)
+			master.mav.global_position_int_send(int(time.time()*1000-start_time), lat*1E7, lng*1E7, alt*1000, 0, 0, 0, 0, 0)
 
 			counter += 1
 			last_update = time.time()
